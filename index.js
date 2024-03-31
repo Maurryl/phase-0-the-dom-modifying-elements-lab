00201ccb5028f307bd34af592044ad8bff79a84a
Cheryl <mutanucheryl77@gmail.com>
2024-04-01 00:38:15 +0300

    add files

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let testVar = "hi"
+
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Cheryl Mutanu is the champion!";
